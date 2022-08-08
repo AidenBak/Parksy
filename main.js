983dd36f7875a0a08fce358ab9066a536ac83e0e
Aiden <sydea000@gmail.com>
2022-08-08 01:08:23 -0500

    15th commit

--- main.js
+++ main.js
@@ -6,3 +6,13 @@ togglebtn.addEventListener('click', () => {
     menu.classList.toggle('active');
     icons.classList.toggle('active');
 });
+
+document.querySelector('.btn2').addEventListener('click',function(){
+    document.querySelector('.main').style.transform = 'translate(-100vw)';
+});
+document.querySelector('.btn3').addEventListener('click',function(){
+    document.querySelector('.main').style.transform = 'translate(-200vw)';
+});
+document.querySelector('.btn1').addEventListener('click',function(){
+    document.querySelector('.main').style.transform = 'translate(0vw)';
+});
